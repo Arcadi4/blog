a9a4e32c39a548db981c2be0f205dea85434e336
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-02-15 18:39:18 -0500

    feat: Update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,10 +7,15 @@ export default function NotFound() {
     const pathname = usePathname();
 
     return (
-        <main className="min-h-dvh flex flex-col md:flex-row">
-            <section className="bg-acid border-b-2 border-b-black md:border-b-0 md:border-r-2 md:border-r-black md:w-2/5">
-                <div className="flex flex-col justify-between h-full p-4">
-                    <div className="flex flex-col">
+        <main className="min-h-dvh flex flex-col 
+                        h-max
+                        md:flex-row md:h-dvh">
+            {/* <div className="absolute h-60 w-12 bg-magenta z-11" />
+            <div className="absolute bottom-0 h-full w-12 bg-klein z-10" /> */}
+            <section className="bg-acid border-b-2 border-b-black
+                md:h-dvh md:border-b-0 md:border-r-2 md:border-r-black md:w-2/5">
+                <div className="flex flex-col justify-between h-full p-4 gap-16">
+                    <div className="flex flex-col z-20">
                         {Array.from({ length: 5 }, (_, index) => (
                             <p key={index} className="font-mono large-p">{pathname}</p>
                         ))}
@@ -20,8 +25,11 @@ export default function NotFound() {
                     </h1>
                 </div>
             </section>
-            <section className="relative bg-white flex-1">
-                <div className="absolute py-4 pl-6 bottom-0">
+            <section className="relative bg-white flex-1
+                md:h-dvh">
+                <div className="absolute
+                    top-0 py-4 pl-6
+                    md:bottom-0 md:top-auto">
                     <div className="flex flex-col gap-4">
                         <Link href="/" className="large-link">
                             Home
