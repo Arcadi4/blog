c08880a21970a2c9f0bd3b1820462a9a51555e62
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-02-27 05:00:10 -0500

    feat: Redesign 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,60 +1,110 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Link from "@/components/Link";
+import { menuItemsErrorPage } from "@/app/posts/menuItems";
+import ProximityLink from "@/components/ProximityLink";
 
 export default function NotFound() {
-    const pathname = usePathname();
+  const pathname = usePathname();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const currentDate =
+    year +
+    "-" +
+    String(month).padStart(2, "0") +
+    "-" +
+    String(day).padStart(2, "0");
+  const currentTime = date.toLocaleTimeString();
 
-    return (
-        <main className="min-h-dvh flex flex-col 
-                        h-max
-                        md:flex-row md:h-dvh">
-            {/* <div className="absolute h-60 w-12 bg-magenta z-11" />
-            <div className="absolute bottom-0 h-full w-12 bg-klein z-10" /> */}
-            <section className="bg-acid border-b-2 border-b-black
-                md:h-dvh md:border-b-0 md:border-r-2 md:border-r-black md:w-2/5">
-                <div className="flex flex-col justify-between h-full p-4 gap-16">
-                    <div className="flex flex-col z-20">
-                        {Array.from({ length: 5 }, (_, index) => (
-                            <p key={index} className="font-mono large-p">{pathname}</p>
-                        ))}
-                    </div>
-                    <h1 className="h1-hero left-2">
-                        404<br />Not Found
-                    </h1>
-                </div>
-            </section>
-            <section className="relative bg-white flex-1
-                md:h-dvh">
-                <div className="absolute
-                    top-0 py-4 pl-6
-                    md:bottom-0 md:top-auto">
-                    <div className="flex flex-col gap-4">
-                        <Link href="/" className="large-link">
-                            Home
-                        </Link>
-                        <Link href="/all" className="large-link">
-                            All Posts
-                        </Link>
-                        <Link href="/about" className="large-link">
-                            About
-                        </Link>
-                        <Link href="/tags" className="large-link">
-                            Tags
-                        </Link>
-                        <Link href="/projects" className="large-link">
-                            Projects
-                        </Link>
-                        <Link href="/links" className="large-link">
-                            Links
-                        </Link>
-                        <Link href="/contact" className="large-link">
-                            Contact
-                        </Link>
-                    </div>
-                </div>
-            </section>
-        </main>
-    );
+  return (
+    <main className="overflow-hidden">
+      {/*<p className="absolute font-title font-bold text-9xl text-stroke leading-none top-0 right-0 select-none pointer-events-none">*/}
+      {/*  404*/}
+      {/*</p>*/}
+      {/*<p className="absolute font-title font-bold text-9xl text-stroke leading-none inset-y-1/2 right-0 select-none pointer-events-none">*/}
+      {/*  Error*/}
+      {/*</p>*/}
+      {/*<p className="absolute font-title font-bold text-9xl text-stroke leading-none bottom-0 right-0 select-none pointer-events-none">*/}
+      {/*  Not Found*/}
+      {/*</p>*/}
+
+      <div className="absolute w-full top-2/3 border-t-black border-t-2 z-10" />
+      <div className="absolute w-full h-12 top-2/3 bg-magenta" />
+      <div
+        className="
+        absolute bg-acid -z-10
+        md:h-dvh md:border-b-0 md:w-1/3 md:min-w-96"
+      />
+      <div
+        className="
+        absolute border-b-2 border-b-black z-10
+        md:h-dvh md:border-b-0 md:border-r-2 md:border-r-black md:w-1/3 md:min-w-96"
+      />
+      <div className="flex flex-row h-screen w-full p-6 gap-6">
+        <div className="flex-1 h-full relative">
+          <div className="flex flex-col z-20 gap-1 absolute left-0 top-0">
+            {Array.from({ length: 5 }, (_, index) => (
+              <p key={index} className="font-mono large-p">
+                {pathname}
+              </p>
+            ))}
+          </div>
+          <div className="flex flex-col z-20 gap-1 absolute left-0 bottom-[calc(1/3*100%+.5rem)]">
+            {Array.from({ length: 5 }, (_, index) => (
+              <p key={index} className="font-mono large-p">
+                {pathname}
+              </p>
+            ))}
+          </div>
+          <pre className="large-p absolute left-0 bottom-0">4xx|404</pre>
+        </div>
+        <div className="flex-1 h-full relative">
+          <div className="flex flex-col items-end absolute right-0 top-[calc(2/3*100%-6*3rem+.5rem)]">
+            {menuItemsErrorPage.map((menuItem, index) => {
+              return (
+                <ProximityLink
+                  href={menuItem.href}
+                  className="large-link select-none"
+                  key={index}
+                >
+                  {menuItem.name}
+                </ProximityLink>
+              );
+            })}
+          </div>
+          <pre className="large-p absolute left-0 bottom-0">not found</pre>
+          <pre className="large-p absolute left-0 top-0">
+            Server returned
+            <br />
+            an error :(
+          </pre>
+        </div>
+        <div className="flex-1 h-full relative">
+          <div className="absolute left-0 top-0 -translate-x-1/2 -translate-y-1/6 text-[1280px] font-medium font-serif text-stroke -z-10 pointer-events-none select-none">
+            *
+          </div>
+          <div className="absolute bottom-1/3">
+            <h1 className="h1-hero w-fit relative">
+              404
+              <div className="w-full h-screen bg-klein absolute bottom-full" />
+            </h1>
+            <h1 className="h1-hero">
+              Not
+              <br />
+              Found
+            </h1>
+          </div>
+          <pre className="large-p absolute left-0 bottom-0">&gt;&gt;&gt;</pre>
+          <pre className="large-p absolute right-0 bottom-0">&gt;&gt;&gt;</pre>
+          <pre className="large-p absolute right-0 top-0 text-right">
+            {currentDate}
+            <br />
+            {currentTime}
+          </pre>
+        </div>
+      </div>
+    </main>
+  );
 }
--- src/app/posts/menuItems.ts
+++ src/app/posts/menuItems.ts
@@ -1,9 +1,23 @@
+export const emptyItem = { name: "\n", href: "" };
+
 export const menuItems = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
   { name: "Contact", href: "/contact" },
   { name: "Projects", href: "/projects" },
   { name: "Friend Links", href: "/links" },
+  { name: "Tags", href: "/tags" },
+  { name: "All Posts", href: "/all" },
+];
+
+export const menuItemsErrorPage = [
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+  { name: "Projects", href: "/projects" },
+  { name: "Friend Links", href: "/links" },
+  emptyItem,
+  { name: "Home", href: "/" },
+  emptyItem,
   { name: "Tags", href: "/tags" },
   { name: "All Posts", href: "/all" },
 ];
