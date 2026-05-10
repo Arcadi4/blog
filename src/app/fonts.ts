e8fe1ac9b8f1052401f78c7411707433ca0e9dbb
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-05-10 14:27:04 -0400

    chore(fonts): add Geist font

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,23 +1,30 @@
 import {
-  Bricolage_Grotesque,
-  IBM_Plex_Sans,
-  Playfair_Display,
+    Bricolage_Grotesque,
+    IBM_Plex_Sans,
+    Playfair_Display,
+    Geist,
 } from "next/font/google";
 
 export const bricolageGrotesque = Bricolage_Grotesque({
-  subsets: ["latin"],
-  variable: "--font-display",
-  weight: "variable",
+    subsets: ["latin"],
+    variable: "--font-display",
+    weight: "variable",
 });
 
 export const ibmPlexSans = IBM_Plex_Sans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-  weight: "variable",
+    subsets: ["latin"],
+    variable: "--font-sans",
+    weight: "variable",
 });
 
 export const playfairDisplay = Playfair_Display({
-  subsets: ["latin"],
-  variable: "--font-serif",
-  weight: ["400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"],
+    variable: "--font-serif",
+    weight: ["400", "500", "600", "700", "800", "900"],
+});
+
+export const geist = Geist({
+    subsets: ["latin"],
+    variable: "--font-title",
+    weight: "variable",
 });
