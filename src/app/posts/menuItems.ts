fdb2bfe695bce891676bbad106a6ae628da7a668
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-07-07 17:55:07 -0400

    refactor(menu): clean up menu items

--- src/app/posts/menuItems.ts
+++ src/app/posts/menuItems.ts
@@ -3,22 +3,9 @@ const emptyItem = { name: "\n", href: "" };
 const menuItems = [
   { name: "Home", href: "/" },
   { name: "About", href: "/about" },
-  { name: "Contact", href: "/contact" },
-  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/about#contact" },
+  { name: "Projects", href: "/about#projects" },
   { name: "Friend Links", href: "/links" },
-  { name: "Tags", href: "/tags" },
-  { name: "All Posts", href: "/all" },
-];
-
-const menuItemsErrorPage = [
-  { name: "About", href: "/about" },
-  { name: "Contact", href: "/contact" },
-  { name: "Projects", href: "/projects" },
-  { name: "Friend Links", href: "/links" },
-  emptyItem,
-  { name: "Home", href: "/" },
-  emptyItem,
-  { name: "Tags", href: "/tags" },
   { name: "All Posts", href: "/all" },
 ];
 
@@ -33,10 +20,4 @@ type LinkItem = {
   href: string;
 };
 
-export {
-  emptyItem,
-  menuItems,
-  menuItemsErrorPage,
-  socialMediaItems,
-  type LinkItem,
-};
+export { emptyItem, menuItems, socialMediaItems, type LinkItem };
